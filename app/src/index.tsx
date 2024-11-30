741201f25c4558cb64b9aa42d22697252eea0f4a
TechbyAshgit branch -M maingit branch -M maingit config --list <166682386+TechbyAsh@users.noreply.github.com>
2024-11-29 23:02:25 -0500

    Layout for Search and list

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View, StyleSheet, } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, Platform } from "react-native";
+import React from "react";     
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <>
+    <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Text> search </Text>
     </View>
+    <View style = {styles.list}>
+      <Text> List </Text>
+    </View>
+    </SafeAreaView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+  search:{
+    backgroundColor: 'green',
+    padding: 16
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+
+  }
+
+
+
+})
