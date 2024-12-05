a5902795b18e714e81a4187132dfb1483a911e2d
TechbyAshgit branch -M maingit branch -M maingit config --list <166682386+TechbyAsh@users.noreply.github.com>
2024-12-04 20:37:49 -0500

    Resturant screen and card component layed out + organzied directires

---
+++ app/src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,40 @@
+import { Text, View, StyleSheet, SafeAreaView,} from "react-native";
+import React from "react";    
+import { Searchbar } from 'react-native-paper';
+import { useState } from "react";
+
+import {RestaurantCardInfo} from '../components/restaurants-info.component'
+
+
+export const RestaurantsScreen = () => {
+    const [searchQuery, setSearchQuery] = React.useState('');
+return ( 
+<SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+    <Searchbar
+     placeholder="Search"
+     onChangeText={setSearchQuery}
+     value={searchQuery}
+   />
+   </View>
+   <View style = {styles.list}>
+     <RestaurantCardInfo />
+   </View>
+   </SafeAreaView>
+   )}; 
+
+   const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+    },
+    search:{
+      backgroundColor: 'green',
+      padding: 16
+    },
+    list: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: 'silver',
+  
+    }
+   })  
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,49 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View, StyleSheet, SafeAreaView, Platform } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView,} from "react-native";
 import React from "react";    
-import { Searchbar } from 'react-native-paper';
-import { useState } from "react";
 
+
+import {RestaurantsScreen} from './features/restaurants/screens/restaurants.screen'
 
 
 
 export default function Index() {
-  const [searchQuery, setSearchQuery] = React.useState('');
+  
 
   return (
     <>
-    <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-     <Searchbar
-      placeholder="Search"
-      onChangeText={setSearchQuery}
-      value={searchQuery}
-    />
-    </View>
-    <View style = {styles.list}>
-      <Text> List </Text>
-    </View>
-    </SafeAreaView>
+    <RestaurantsScreen />
     <StatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-  },
-  search:{
-    backgroundColor: 'green',
-    padding: 16
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-
-  }
 
 
 
-})
