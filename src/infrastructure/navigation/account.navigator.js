96dd6495f0046043b1b24a6a506cddfb3f6c63de
TechbyAshgit branch -M maingit branch -M maingit config --list <166682386+TechbyAsh@users.noreply.github.com>
2025-01-20 19:56:52 -0500

    Authication and Login screens set up

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+
+import { AccountScreen } from '../../features/account/screens/account.screen';
+import { LoginScreen } from '../../features/account/screens/login.screen';
+import { RegisterScreen } from '../../features/account/screens/register.screen';
 
 const Stack = createStackNavigator();
 
@@ -8,19 +11,15 @@ export const AccountNavigator = () => (
   <Stack.Navigator screenOptions={{headerShown: false, }}>
     <Stack.Screen
       name="Main"
-      component={() => (
-        <View>
-          <Text>Account Screen</Text>
-        </View>
-      )}
+      component={AccountScreen}
     />
     <Stack.Screen
       name="Login"
-      component={() => (
-        <View>
-          <Text>Login Screen</Text>
-        </View>
-      )}
+      component={LoginScreen}
+    />
+    <Stack.Screen
+      name="Register"
+      component={RegisterScreen }
     />
   </Stack.Navigator>
 );
